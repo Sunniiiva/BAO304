ac67b68517fdbae9151e5be23973b6d4060d06d0
jamz783 <israjama04@gmail.com>
2026-01-27 12:04:33 +0100

    27.01.26
    pusher endringer (kommentar for hva som skal bli gjort)

--- src/patch/fetch_patch.py
+++ src/patch/fetch_patch.py
@@ -3,5 +3,5 @@
 #parse patch (added/removed/context)
 #lagre before-code og after-code
 #lage en strukturert representasjon av hver fil med antall added/removed lines, hunks, språk
-#output: repo_url, commit_hash, file_path, added_lines, removed_lines, patch_text, before_code, after_code, language
+#output: repo_url, commit_hawsh, file_path, added_lines, removed_lines, patch_text, before_code, after_code, language
 
