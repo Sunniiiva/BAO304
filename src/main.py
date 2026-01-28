7556ab080045f827ee122e2596824302dd92f3aa
coduck2004 <hermannhyh2004@gmail.com>
2026-01-29 00:13:05 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+from cve import fetch_cve
+from repo import fetch_commit
+from patch import fetch_patch
 
-# kalle moduler i rekkefølge: fetch_cve, fetch_commit, fetch_patch, database
+
+# kalle moduler i rekkefølge: fetch_cve, fetch_commit, fetch_patch, 
+
+fetch_cve.load_cve_from_file(file)
+
+
 # logging og feilhåntering
