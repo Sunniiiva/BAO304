0aab0817984df604f59329a476b6a891154968dd
coduck2004 <hermannhyh2004@gmail.com>
2026-02-01 23:03:08 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 from cve import fetch_cve
 from repo import fetch_commit
-from patch import fetch_patch
+
 import typer 
 
 app = typer.Typer()
 
-#kode som kjøres uten parametere
+# kode som kjøres dersom parametere ikke er gitt
 
 @app.callback(invoke_without_command=True)
 def main(ctx: typer.Context):
     if ctx.invoked_subcommand is None:
         typer.echo("Ingen kommando er gitt. bruk --help for mer informasjon.")
         raise typer.Exit(code=0)
-
+#test kode
 @app.command()
 def hello():
+    """Sier hello (testkommando)"""
     print("hello")
+
+# kalle moduler i rekkefølge: fetch_cve, fetch_commit, fetch_patch.
+@app.command()
+def kallmoduler():
+    """kaller fetch_cve, fetch_commit, fetch_patch"""
     
+
+
+
 
 if __name__ == "__main__":
     app()
-# kalle moduler i rekkefølge: fetch_cve, fetch_commit, fetch_patch, 
+ 
 
 
 
